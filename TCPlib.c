c0bfbb2a39eb868e893158cf5fff59302657fbe4
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2014-02-28 11:26:25 +0000

    Wrote TCPrecv() and TCPcreate().

--- TCPlib.c
+++ TCPlib.c
@@ -45,6 +45,7 @@ int TCPsend(int fd, char *message, unsigned int len)
 	SIGPIPE signal and, by default, that would kill your process.
 
 	idea: ignore SIGPIPE and then reactivate it after all writes */
+	nleft = len;
 	while(nleft>0)
 	{
 		nwritten = write(fd, ptr, nleft);
@@ -58,12 +59,25 @@ int TCPsend(int fd, char *message, unsigned int len)
 }
 
 
-TCPrecv()
+/* reads content from fd and writes at most len bytes to the location starting at str. retuns the number of bytes written to str. returns -1 on unsuccessful read. retuns -2 if connection is closed by peer */
+int TCPrecv(int fd, char *str, unsigned int len)
 {
+	char *ptr = str;
+	int nread, nleft;
 
+	nleft = len;
+	while(nleft>0)
+	{
+		nread = read(fd, ptr, nleft);
+		if(nread==-1)
+			return -1;	/* error while reading */
+		else if(nread==0)
+			return -2;	/* connection closed by peer */
+		nleft -= nread;
+		ptr += nread;
+	}
 
-
-
+	return nread = len - nleft;
 }
 
 /* closes the file descriptor fd. retuns 0 on success. returns -1 on error and sets errno */
@@ -72,3 +86,28 @@ int TCPclose(int fd)
 	return close(fd);
 }
 
+/* binds IP and port to the returned fd. when failing to create a socket returns -1; when failing to connect returns -2. sets errno in both cases. */
+int TCPcreate(unsigned long IP, unsigned short port)
+{
+	int fd;
+	struct sockaddr_in address;
+
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(fd==-1)
+	{
+		return -1;
+	}
+
+	memset(&address, 0, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = htonl(IP);
+	address.sin_port = htons(port);
+
+	/* struct sockaddr_in is the same size as struct sockaddr */
+	if(bind(fd, (struct sockaddr*)&address, sizeof(address))==-1)
+	{
+		return -2;
+	}
+
+	return fd;
+}
--- TCPlib.h
+++ TCPlib.h
@@ -3,7 +3,8 @@
 
 int TCPconnect(unsigned long IP, unsigned short port);
 int TCPsend(int fd, char *message, unsigned int len);
-
+int TCPrecv(int fd, char *str, unsigned int len);
 int TCPclose(int fd);
+int TCPcreate(unsigned long IP, unsigned short port);
 
 #endif
